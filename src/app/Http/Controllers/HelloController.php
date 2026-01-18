9a8a068c43c5c62d3506381cb1270c14e0b5360c
k-masudaIC <k.masuda@c-innocent.com>
2026-01-18 13:06:28 +0900

    アクション追加、ルート作成

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index(){
+
+    return <<<E0F
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body  { font-size:16pt; color:#999; }
+    h1   { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+    </style>
+    </head>
+    <body>
+    <h1>Hello</h1>
+    <p>This is HelloController index method.</p>
+    <p>これは、サンプルページです。</p>
+    </body>
+    </html>
+    E0F;
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,50 +1,31 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<E0F
-<html>
-<head>
-<title>Hello</title>
-<style>
-body  { font-size:16pt; color:#999; }
-h1   { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-<h1>Hello</h1>
-<p>This is sample page.</p>
-<p>これは、サンプルページです。</p>
-</body>
-</html>
-E0F;
+Route::get('hello', [HelloController::class, 'index']);
 
-//http://localhost:8000/helloで確認。
-//ルート作成はここでできることを覚えておく。
-Route::get('hello', function () use ($html) {
-    return $html;
-});
+// Route::get('hello/{msg?}', function ($msg) {
+//     $html = <<<E0F
+//     <html>
+//     <head>
+//     <title>Hello</title>
+//     <style>
+//     body  { font-size:16pt; color:#999; }
+//     h1   { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+//     </style>
+//     </head>
+//     <body>
+//     <h1>Hello</h1>
+//     <p>{$msg}</p>
+//     <p>これは、サンプルページです。</p>
+//     </body>
+//     </html>
+//     E0F;
+//     return $html;
+// });
 
-Route::get('hello/{msg?}', function ($msg) {
-    $html = <<<E0F
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body  { font-size:16pt; color:#999; }
-    h1   { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-    </style>
-    </head>
-    <body>
-    <h1>Hello</h1>
-    <p>{$msg}</p>
-    <p>これは、サンプルページです。</p>
-    </body>
-    </html>
-    E0F;
-    return $html;
-});
