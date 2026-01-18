68d77783fb6dc0d372b7735fc309c89da5aa9b2b
k-masudaIC <k.masuda@c-innocent.com>
2026-01-18 13:17:22 +0900

    複数アクションの利用

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -4,28 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOT
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+</style>
+EOT;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt){
+    return "<{$tag}>{$txt}</{$tag}>";
+}
+
 class HelloController extends Controller
 {
-    public function index($id='noname', $pass='unknown')
+    public function index()
     {
+        global $head, $style, $body, $end;
+        $html = $head . tag('title', 'Hello/Index') . $style . $body
+        . tag('h1', 'Hello Index')
+        . tag('p', 'This is HelloController@index method')
+        .'<a href="/hello/other">Go to Other</a>'
+        . $end;
+        return $html;
+    }
 
-    return <<<E0F
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body  { font-size:16pt; color:#999; }
-    h1   { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-    </style>
-    </head>
-    <body>
-    <h1>Hello</h1>
-    <p>This is HelloController index method.</p>
-    <p>ID: {$id}</p>
-    <p>Password: {$pass}</p>
-    <p>これは、サンプルページです。</p>
-    </body>
-    </html>
-    E0F;
+    public function other()
+    {
+        global $head, $style, $body, $end;
+        $html = $head . tag('title', 'Hello/Other') . $style . $body
+        . tag('h1', 'Other')
+        . tag('p', 'this is other page')
+        .'<a href="/hello">Go to Index</a>'
+        . $end;
+        return $html;
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -7,25 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{id?}/{pass?}', [HelloController::class, 'index']);
-
-// Route::get('hello/{msg?}', function ($msg) {
-//     $html = <<<E0F
-//     <html>
-//     <head>
-//     <title>Hello</title>
-//     <style>
-//     body  { font-size:16pt; color:#999; }
-//     h1   { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-//     </style>
-//     </head>
-//     <body>
-//     <h1>Hello</h1>
-//     <p>{$msg}</p>
-//     <p>これは、サンプルページです。</p>
-//     </body>
-//     </html>
-//     E0F;
-//     return $html;
-// });
-
+Route::get('hello', [HelloController::class, 'index']);
+Route::get('hello/other', [HelloController::class, 'other']);
