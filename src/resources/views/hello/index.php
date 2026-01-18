288075b88f5564f5cdf44523ceaae21136aa07db
k-masudaIC <k.masuda@c-innocent.com>
2026-01-18 13:41:16 +0900

    PHPでテンプレート作成し、ルート設定する

---
+++ src/resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Hello</title>
+    <style>
+        body { font-size:16pt; color:#999; }
+        h1 { font-size:100pt; text-align:right; color:#fafafa; 
+        margin:-50px 0px -120px 0px; }
+    </style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>This is the hello index page.</p>
+</body>
+</html>
