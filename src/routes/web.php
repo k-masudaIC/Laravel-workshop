dd79e8a9ed1ce222a8125edcb94bd519985b4db6
k-masudaIC <k.masuda@c-innocent.com>
2026-01-18 12:43:34 +0900

    ルート情報を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//http://localhost:8000/helloで確認。
+//ルート作成はここでできることを覚えておく。
+Route::get('hello', function () {
+    return '<html><body><h1>Hello, World!</h1><p>This is a sample page.</p></body></html>';
+});
