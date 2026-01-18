d56087ecad97df60c61b3a52b065d4146d3b6bcf
k-masudaIC <k.masuda@c-innocent.com>
2026-01-18 13:34:28 +0900

    RequestおよびResponse

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -3,24 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function __invoke()
+    public function index(Request $request, Response $response)
     {
-        return <<<EOF
+        $html = <<<E0F
         <html>
-        <head><title>Hello</title>
+        <head>
+        <title>Hello</title>
         <style>
         body { font-size:16pt; color:#999; }
-        h1 { font-size:100pt; text-align:right; color:#eee; }
-        </style>
+        h1 { font-size:100pt; text-align:right; color:#fafafa; 
+        margin:-50px 0px -120px 0px; }
         </head>
         <body>
-        <h1>Single Action</h1>
-        <p>これはシングルアクションコントローラのアクションです。</p>
+        <h1>Hello</h1>
+        <h3>Request</h3>
+        <pre>{$request}</pre>
+        <h3>Response</h3>
+        <pre>{$response}</pre> 
         </body>
         </html>
-        EOF; 
+        E0F;
+
+        $response->setContent($html);
+        return $response;
     }
+
 }
