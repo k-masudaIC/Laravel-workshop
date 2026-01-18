a59f5d9a1e6ac1e48bfec1a6d38c836cd613db96
k-masudaIC <k.masuda@c-innocent.com>
2026-01-18 13:42:53 +0900

    コントローラーでテンプレートを使う

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -7,29 +7,9 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index()
     {
-        $html = <<<E0F
-        <html>
-        <head>
-        <title>Hello</title>
-        <style>
-        body { font-size:16pt; color:#999; }
-        h1 { font-size:100pt; text-align:right; color:#fafafa; 
-        margin:-50px 0px -120px 0px; }
-        </head>
-        <body>
-        <h1>Hello</h1>
-        <h3>Request</h3>
-        <pre>{$request}</pre>
-        <h3>Response</h3>
-        <pre>{$response}</pre> 
-        </body>
-        </html>
-        E0F;
-
-        $response->setContent($html);
-        return $response;
+        return view('hello.index');
     }
 
 }
