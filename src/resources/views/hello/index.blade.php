11d4602267b5366bce380cd1ef0f912c24dc8660
k-masudaIC <k.masuda@c-innocent.com>
2026-01-19 19:40:30 +0900

    for continue break loopの書き方

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -1,19 +1,48 @@
 <html>
+
 <head>
     <title>Hello</title>
     <style>
-        body { font-size:16pt; color:#999; }
-        h1 { font-size:50pt; text-align:right; color:#f6f6f6; 
-        margin:-20px 0px -30px 0px; letter-spacing: -4pt ;}
+        body {
+            font-size: 16pt;
+            color: #999;
+        }
+
+        h1 {
+            font-size: 50pt;
+            text-align: right;
+            color: #f6f6f6;
+            margin: -20px 0px -30px 0px;
+            letter-spacing: -4pt;
+        }
     </style>
 </head>
+
 <body>
     <h1>Blade/Index</h1>
-    <p>&#064;foreachディレクティブの例</p>
+    <p>&#064;forディレクティブの例</p>
+    <ol>
+        @for ($i = 0; $i < count($data); $i++)
+        @if( $i % 2==1 )
+            @continue
+        @elseif($i <=10)
+            <li>No. {{ $data[$i] }}</li>
+        @else
+            @break
+        @endif
+        @endfor
+    </ol>
+    <p>&#064;$loopの例</p>
     <ol>
         @foreach ($data as $item)
-            <li>{{ $item }}</li>
+        @if( $loop->first)
+        <p>-- start --</p>
+        @endif
+            <li>No. {{$loop->iteration}}. {{ $item }}</li>
+        @if( $loop->last)
+        <p>-- end --</p>
+        @endif
         @endforeach
-    </ol>
 </body>
+
 </html>
