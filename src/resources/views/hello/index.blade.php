0c27759b8c3fa64e6bc5a0c529132926d23bf5a5
k-masudaIC <k.masuda@c-innocent.com>
2026-01-19 19:27:36 +0900

    foreach文の利用

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -9,15 +9,11 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    @if($msg != '')
-    <p>こんにちは：{{ $msg }}さん</p>
-    @else
-    <p>何か書いてください</p>
-    @endif    
-    <form method="POST" action="/hello">
-        @csrf
-        <input type="text" name="msg">
-        <input type="submit" value="送信">
-    </form>
+    <p>&#064;foreachディレクティブの例</p>
+    <ol>
+        @foreach ($data as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+    </ol>
 </body>
 </html>
