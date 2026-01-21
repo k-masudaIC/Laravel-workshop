bb419ee1053bc9568b8ca0619c15cac0ca5130b8
k-masudaIC <k.masuda@c-innocent.com>
2026-01-21 19:15:27 +0900

    docker-compose exec app php artisan optimize:clear docker-compose exec app php artisan config:clear docker-compose exec app php artisan cache:clear docker-compose exec app composer dump-autoload　しないとbootメソッドが正しく呼ばれない

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -9,12 +9,7 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data=[
-            ['name' => '山田太郎', 'mail' => 'taro@yamada'],
-            ['name' => '鈴木次郎', 'mail' => 'jiro@suzuki'],
-            ['name' => '佐藤三郎', 'mail' => 'saburo@sato']
-        ];
-        return view('hello.index', ['data' => $data]);
+        return view('hello.index', ['message' => 'Hello!']);
     }
 
     public function post(Request $request)
