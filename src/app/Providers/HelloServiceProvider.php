0aac71a28475d6d74150c2d83f9ccb3543251f10
k-masudaIC <k.masuda@c-innocent.com>
2026-01-21 19:07:32 +0900

    クロージャでコンポーザ処理作成

--- src/app/Providers/HelloServiceProvider.php
+++ src/app/Providers/HelloServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('hello.index', function ($view) {
+            $view->with('view_message', 'Composer message!');
+        });
     }
 }
