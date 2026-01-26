e8f80172175afd4079c3c3e6e35dc9f58bfd3d23
iida775 <r.iida@c-innocent.com>
2026-01-27 04:33:23 +0900

    DB::selectの利用

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,19 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\DB;
 
 
 class HelloController extends Controller
 {
-    use ValidatesRequests;
     public function index(Request $request)
     {
-        return view('hello.index', ['msg' => 'フォームを入力:']);
-    }
-
-    public function post(HelloRequest $request)
-    {
-        return view('hello.index', ['msg' => '正しく入力されました。']);
+        $items = DB::select('SELECT * FROM people');
+        return view('hello.index', ['items' => $items]);
     }
 }
 
