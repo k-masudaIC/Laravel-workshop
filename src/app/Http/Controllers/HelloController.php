7c6bd0607c0d1f21cca8370252ebad1a2369e10e
iida775 <r.iida@c-innocent.com>
2026-01-27 14:51:15 +0900

    insertによるデータの挿入

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -13,15 +13,32 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if(isset($request->id))
-        {
-            $param = ['id' => $request->id];
-            $item = DB::select('select * from people where id = :id', $param);
-        }else{
-            $item = DB::select('select * from people');
-        }
-        return view('hello.index', ['items' => $item]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+public function post(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }   
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
     }
 }
+
 
 
